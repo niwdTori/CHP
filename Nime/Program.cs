aa10ba1d64ea04e598171152a47618f88c9dc2af
Tori <mes.viktoria@mail.ru>
2022-08-19 03:29:40 +0500

    Nime

---
+++ Nime/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "tori")
+{
+    Console.WriteLine("Няяя, ето Тори<3");
+}
+else
+{
+    Console.Write("Халлоууу, ");
+    Console.Write(username);
+}
