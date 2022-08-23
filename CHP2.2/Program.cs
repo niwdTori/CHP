528915c14e531f92918b2f462c47f79885e6141d
Tori <mes.viktoria@mail.ru>
2022-08-23 21:28:11 +0500

    A lot of things

---
+++ CHP2.2/Program.cs
@@ -0,0 +1,65 @@
+﻿// int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+// array[0] = 12;
+// Console.WriteLine(array[4]);
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; //создает новый массив, в которм 10 элементов
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 14);
+Console.WriteLine(pos);
+
+// int n = array.Length;
+// int find = 18;
+
+// int index = 0;
+// while (index < n)
+// {
+//     if(array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     index++;
+// }
