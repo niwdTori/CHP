88c00848b9c00aa0736ce3042375fd3883df221b
Tori <mes.viktoria@mail.ru>
2022-09-08 01:22:49 +0500

    Фибоначи

---
+++ CHP4.2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Факториал n
+// int Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// Console.WriteLine(Factorial(5)); 
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if ( n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Fibonacci(i)}");
+}
